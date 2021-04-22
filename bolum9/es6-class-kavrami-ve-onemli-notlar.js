0501d23e66c598cbc2bfc3f53b1ee37026cf43cd
burak2043@gmail.com <73242451+brkckr20@users.noreply.github.com>
2021-04-22 09:23:37 +0300

    es6 dosyası güncellendi ve son haline getirild

--- bolum9/es6-class-kavrami-ve-onemli-notlar.js
+++ bolum9/es6-class-kavrami-ve-onemli-notlar.js
@@ -3,24 +3,49 @@
 
 
 class Person{
+
+    static sayac = 0;
+
     constructor(ad,soyad){
         this.adi    = ad;
         this.soyadi = soyad;
+        Person.sayac++;
+        /**
+         * statik özelliklere nesneler üzeründen değil sınıflar üzerinden erişilir.
+         */
     }
 
     selamVer(){
         return `Merhaba ben ${this.adi + " " + this.soyadi}`;
     }
+
+
+    /**
+     *  statik değişkenler oluşturulabildiği gibi static fonksiyonlarda oluşturulabilir.
+     */
+    static test () {
+        console.log("Bu çalışan metod static bir metotdur.");
+    }
 }   
 
 class Ogrenci extends Person{
-    constructor(ad,soyad,yas){
+    constructor(ad,soyad,sinif){
 /*         this.ad = ad;
         this.soyad = soyad; */ //bunları üst sınıfta da kullandığımız için kalıtım alınan classın constructorundaki özelliklere erişmek için super() anahtar kelimesi kullanılır.
         super(ad,soyad); //ad ve soyad değerleri aynı işlemler kullanılacağı için git bir üst sınıfta yap demiş olduk. yani super anahtar kelimesi ile bir üst sınıfın contructor metodunu çağırmış olduk.
-        this.yas = yas;
+        this.sinif = sinif;
+    }
+
+
+    sinifiniSoyle() {
+        return `Ben ${this.sinif}. sınıf öğrencisiyim.`;
     }
 }
+/*
+    alt sınıflar, kalıtım alınan üst sınıfların metot ve özelliklerini kullanabilirken üst sınıflar
+    kalıtım vermiş olduğu alt sınıfların metot ve özelliklerini kullanamaz.
+
+*/
 
 
 const emre = new Person('Emre','Çetin');
@@ -29,3 +54,15 @@ console.log(emre.selamVer());
 
 const burak = new Ogrenci("Burak","Çakır");
 console.log(burak.selamVer());
+
+const ayse = new Ogrenci("Ayşe","ÇAKIR",12);
+console.log(ayse.sinifiniSoyle());
+
+
+console.log("----");
+console.log("Oluşturulan person nesnesi sayısı  : " + Person.sayac);
+
+// statik metod çağrılması
+Person.test();
+//statik metod örneği 
+Math.random();
