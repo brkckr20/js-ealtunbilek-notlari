39555ab1746406639280cffc6c9dbb8b09f76c42
burak2043@gmail.com <73242451+brkckr20@users.noreply.github.com>
2021-05-18 17:58:17 +0300

    bolum17 npm kavrami basladi

---
+++ bolum17-npm-kavrami-ve-detaylari/npm-komutlari.js
@@ -0,0 +1,5 @@
+/**
+ *  npm init -> npm kullanmamız için bir package.json dosyası oluşturur. --yes ile tüm sorulara default cevaplar verilir.
+ *  npm i veya npm install -g npm@6.14.4 --> global olarak npm'i ilgili sürüm olarak yükler.
+ *
+ * */
